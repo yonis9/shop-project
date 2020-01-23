e89dcafd407078efd06d908b575ca9c3ce3aa825
Yoni Sisso <yonis9@walla.com>
2020-01-23 19:52:37 +0200

    added redex-persistor to persist cart state

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate  } from 'redux-persist/integration/react'
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 
 import * as serviceWorker from './serviceWorker';
@@ -13,7 +14,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
                 <Provider store={store}>
                   <BrowserRouter>
-                     <App />
+                    <PersistGate persistor={persistor}>
+                      <App />
+                    </PersistGate>
                   </BrowserRouter>
                 </Provider>, document.getElementById('root'));
 
