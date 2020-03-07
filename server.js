bf1536d4bf6a55fbe20c1cb4f0d4a90f7863a4c4
Yoni Sisso <yonis9@walla.com>
2020-03-07 20:31:33 +0200

    updating our code to be pwa compliant, added serviceworker, https and update manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression')
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,20 +12,27 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors())
 
 
+
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression())
+    app.use(enforce.HTTPS({ trustProtoHeader: true }))
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 
 app.post('/payment', (req, res) => {
